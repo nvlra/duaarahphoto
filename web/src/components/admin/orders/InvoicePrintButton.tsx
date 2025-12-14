827dbdaec2e41cdf7e3e21da1bca77264b066098
Jack Sparrow <novalgans1908@gmail.com>
2025-12-14 18:24:04 +0700

    update

--- web/src/components/admin/orders/InvoicePrintButton.tsx
+++ web/src/components/admin/orders/InvoicePrintButton.tsx
@@ -91,7 +91,7 @@ export default function InvoicePrintButton({ orderData }: { orderData: RawOrder 
         </Button>
       </DialogTrigger>
 
-      <DialogContent className="max-w-[95vw] h-[90vh] flex flex-col p-0 bg-slate-100 dark:bg-slate-950 border-none sm:rounded-xl overflow-hidden">
+      <DialogContent className="max-w-[95vw] h-[85vh] flex flex-col p-0 bg-slate-100 dark:bg-slate-950 border-none sm:rounded-xl overflow-hidden">
         <DialogHeader className="px-4 py-3 bg-white dark:bg-slate-900 border-b dark:border-slate-800 flex-none z-10 shadow-sm">
            <div className="flex justify-between items-center w-full">
              <div>
