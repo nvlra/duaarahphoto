d2ff8b1b4053d8a464fe098beebdf05c0735406e
Jack Sparrow <novalgans1908@gmail.com>
2025-12-15 13:06:15 +0700

    update

--- web/src/components/ui/ios-toast.tsx
+++ web/src/components/ui/ios-toast.tsx
@@ -70,7 +70,7 @@ const ToastItem = memo(({ toast, onRemove }: { toast: Toast, onRemove: (id: stri
         className={`
             relative mx-auto min-h-fit w-full overflow-hidden rounded-2xl p-4
             transition-all duration-200 ease-in-out hover:scale-[103%]
-            bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]
+            bg-white/80 backdrop-blur-md [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]
             dark:bg-transparent dark:backdrop-blur-md dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]
         `}
       >
