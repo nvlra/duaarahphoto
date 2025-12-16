265c181f3114f589ef88b252aca77bd0abdb9c26
Jack Sparrow <novalgans1908@gmail.com>
2025-12-16 11:44:56 +0700

    update landing page

--- web/src/components/sections/hero.tsx
+++ web/src/components/sections/hero.tsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 
 export function Hero() {
   return (
-    <section className="bg-background pt-32 pb-10 px-4 md:px-8">
+    <section className="bg-background pt-24 pb-10 px-6 md:px-12">
       <div className="w-full relative group">
         
         {/* Rounded Image Container */}
